6d2ea5c210b3c4d5e8a3dbfe1e4232ebeac524ba
zhangfuling <holo@holos-MacBook-Pro.local>
2021-10-21 14:20:41 +0800

    init

---
+++ src/views/axios.vue
@@ -0,0 +1,18 @@
+<template>axios</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from '../utils/axios'
+
+export default defineComponent({
+  setup() {
+    axios
+      .get('/users/XPoet')
+      .then((res) => {
+        console.log('res: ', res)
+      })
+      .catch((err) => {
+        console.log('err: ', err);
+      })
+  }
+})
+</script>

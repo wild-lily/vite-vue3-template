49b392c2ea7bb7ba542512f1f2ce01ce904d489a
AltmanIss <AltmanIss@users.noreply.github.com>
2021-10-21 09:26:21 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 1d49dd18dd7e54f15738c55805dada87c88aec36

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-// todo
-// import 'element-plus/lib/theme-chalk/index.css'
-
-createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
